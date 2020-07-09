6a5b4bc8426a89b3d54931cf624fe9f9e9f874d4
José Miguel Gutiérrez Guevara <jmiguel_guevara@outlook.com>
2020-07-09 12:59:26 -0500

    The GraphQL respons, only works in bakend scope

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,39 @@
 import {useQuery} from '@apollo/react-hooks'
 import {gql} from 'apollo-boost'
+import Card from '../components/card'
 
 const GET_LAUNCHES = gql`
     {
         launchesPast(limit: 10) {
             mission_name
             launch_date_local
+            launch_date_utc
             links {
                 article_link
                 video_link
                 flickr_images
             }
-            launch_date_utc
         }
     }
 `
 
-const Index = (pops) => {
+const Index = (props) => {
     const {loading, error, data} = useQuery(GET_LAUNCHES)
     if(loading) return <p>Esta cargando ...</p>
-    if(error) return <p>Ha ocurrido un error...</p>
-    console.log(data)
-    return(
-        <div>
-            <h1>This is the new homepage</h1>
-        </div>
-    )
+    if(error) return(<p>Ha ocurrido un error...</p>)
+    try {
+        return(
+            <Card info={data.launchesPast[0]}/>
+        )
+    } catch (error) {
+        return <p>Ha ocurrido un error...</p>
+    } finally{
+        return(
+            <p>Cargo bien la pagina{console.log(props.data)}</p>
+        )
+    }
 }
+
+
 
 export default Index
